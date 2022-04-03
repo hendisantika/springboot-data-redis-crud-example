f6d77524031045192d9bcbf95bb2e5e601c8f9a1
Hendi Santika <hendisantika@gmail.com>
2022-04-03 15:08:09 +0700

    #7 - Add createStudent API in StudentController

--- src/main/java/com/hendisantika/controller/StudentController.java
+++ src/main/java/com/hendisantika/controller/StudentController.java
@@ -1,8 +1,19 @@
 package com.hendisantika.controller;
 
+import com.hendisantika.model.Student;
 import com.hendisantika.repository.StudentRepository;
+import io.swagger.v3.oas.annotations.Operation;
+import io.swagger.v3.oas.annotations.media.ArraySchema;
+import io.swagger.v3.oas.annotations.media.Content;
+import io.swagger.v3.oas.annotations.media.Schema;
+import io.swagger.v3.oas.annotations.responses.ApiResponse;
+import io.swagger.v3.oas.annotations.responses.ApiResponses;
 import io.swagger.v3.oas.annotations.tags.Tag;
 import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.http.HttpStatus;
+import org.springframework.http.ResponseEntity;
+import org.springframework.web.bind.annotation.PostMapping;
+import org.springframework.web.bind.annotation.RequestBody;
 import org.springframework.web.bind.annotation.RestController;
 
 /**
@@ -20,4 +31,17 @@ public class StudentController {
 
     @Autowired
     private StudentRepository studentRepository;
+
+    @Operation(summary = "Get all student", description = "Returns a list of student", operationId = "student")
+    @ApiResponses(value = {
+            @ApiResponse(responseCode = "200", description = "ok, successful operation",
+                    content = @Content(array = @ArraySchema(schema = @Schema(implementation = Student.class)))),
+            @ApiResponse(responseCode = "401", description = "Unauthorized"),
+            @ApiResponse(responseCode = "403", description = "Forbidden"),
+            @ApiResponse(responseCode = "404", description = "Not found")})
+    @PostMapping("/students")
+    public ResponseEntity<Student> createStudent(@RequestBody Student student) {
+        Student savedStudent = studentRepository.save(student);
+        return new ResponseEntity<>(savedStudent, HttpStatus.CREATED);
+    }
 }
